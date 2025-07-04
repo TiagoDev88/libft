458f63be394dbfa91b509abff0785e3074a80701
TiagoDev88 <tiagomonteiro.dev@gmail.com>
2025-07-05 00:27:42 +0100

    Add ft_strcmp function to compare two strings; update Makefile to include new source

--- libft.h
+++ libft.h
@@ -60,6 +60,7 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strdup(const char *s);
+int		ft_strcmp(char *s1, char *s2);
 
 /* ************************************************************************** */
 /*                               Memory Functions                             */
